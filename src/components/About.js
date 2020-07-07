24ec446ed4fcf32fd0a5bb85c39eaabc80316761
System Administrator <jon.s.solari@gmail.com>
2020-07-07 18:08:11 -0400

    added route 53 msg

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,10 @@ export default function About(){
     return(
         <div class="about">
             <h2>What was used to make this project?</h2>
-                <p>Deployment and DNS routing with Amplify</p>
+                <p>Deployment with Amplify</p>
                 <p>Image hosting with S3</p>
                 <p>Groups and users created with IAM</p>
+                <p>DNS routing with Route 53</p>
         </div>
     )
 }
